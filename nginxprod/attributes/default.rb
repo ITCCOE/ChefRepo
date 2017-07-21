f7ca26a26f58d7945af35d6dfecdb33040658494
anurharish <harish.an@itcinfotech.com>
2017-07-21 17:24:48 +0530

    updated

--- nginxprod/attributes/default.rb
+++ nginxprod/attributes/default.rb
@@ -1,2 +1,1 @@
-node.override['rails_app']['git_repo'] = "git@github.com:https://github.com/anurharish/ChefRepo.git"
-#default[:nginxprod][:html_dir] = "/usr/share/nginx/html"
+default[:nginxprod][:html_dir] = "/usr/share/nginx/html"
